ba9782613e9580ac4462241d4035a21c83723b1d
NeTZeN <netzen0su@gmail.com>
2020-06-05 00:48:20 +0500

    Changing the way variables are defined, fixed comments.

--- bisection_method/bisec_mtd.cpp
+++ bisection_method/bisec_mtd.cpp
@@ -7,28 +7,31 @@ float mpd(float x) // functions mpd - "metod polovinnogo deleniya"
     return exp(pow(sin(x),2)); // (3*cos(2*x+4))
 }
 
+const double PI = 3.14159265; // определяем число ПИ
+
 int main()
 {
     setlocale(0, "");
     float a,b,e,x; int k=0;
-    cout<<"1. y=3*cos(2*x+4)\n";
+    cout<<" F(x)=exp(pow(sin(x),2))\n";
     cout<<"Левая граница a=";
-    cin>>a;
+    a = PI/2; // определяем значение левой границы
     cout<<"Правая граница b=";
-    cin>>b;
+    b = 3*PI/2; // определяем значение правой границы
     cout<<"Точность e=";
-    cin>>e;
+    e = 0.05; // определяем точность
     x=(a+b)/2;
     cout<<"Корень уравнения:"<<x;
-    //while(fabs(b-a)>e)
-    //    {
+    while(fabs(b-a)>e)
+        {
             if(mpd(a)*mpd(x)<=0) b=x;
-            else a=x;
+            else 
+                a=x;
                 x=(a+b)/2;
-    //            k++;
-            cout<<"\nТочность: "<<fabs(b-a)<<" Корень уравнения: "<<x; //" Итерация №="<<k<<
-    //    }
+                k++;
+            cout<<"\nТочность: "<<fabs(b-a)<<" Итерация №: " << k << "Корень уравнения: "<< x << endl;; 
+        }
 
-    // cout<<"\nКорень уравнения: "<< x << endl; //"\nКоличество итераций ="<<k<<"
+    cout<<"\nКоличество итераций =" <<k<< " \nКорень уравнения: "<< x << endl; 
     return 0;
 }
